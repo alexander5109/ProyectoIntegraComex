05e91fcff0991fe9d5514bf12cc24784c2a48e8d
ecthelion5109 <xanderseling@gmail.com>
2025-06-29 03:56:21 -0300

    sa

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoComex.Data;
+using ProyectoComex.Models;
+
+namespace ProyectoComex.Controllers {
+	public class ClientesController : Controller {
+		private readonly ComexContext _context;
+		public ClientesController(ComexContext context) {
+			_context = context;
+		}
+
+		public IActionResult Index() {
+
+			List<Cliente> clientes = _context.Clientes.ToList();
+
+
+
+
+			return View(clientes);
+		}
+	}
+}
