288514dcab571b6f5b6187ef42f166d11c83b475
ecthelion5109 <xanderseling@gmail.com>
2025-07-01 06:57:04 -0300

    Agregar archivos de proyecto.

---
+++ Data/ComexContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoComex.Data {
+	public class ComexContext: DbContext {
+		public ComexContext(DbContextOptions<ComexContext> options) {
+
+
+		}
+	}
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoComex.Models {
+	public class Cliente {
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(11, MinimumLength = 11)]
+		public string CUIT { get; set; }
+
+		[Required]
+		public string RazonSocial { get; set; }
+
+		[Phone]
+		public string Telefono { get; set; }
+
+		[StringLength(200)]
+		public string Direccion { get; set; }
+
+		public bool Activo { get; set; }
+	}
+}
