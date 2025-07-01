7f0eb42211421523bc933718c2d1fde86d95534f
ecthelion5109 <xanderseling@gmail.com>
2025-07-01 04:37:56 -0300

    confirmar delete propia funcion

---
+++ wwwroot/js/confirmarDelete.js
@@ -0,0 +1,20 @@
+﻿
+document.addEventListener("DOMContentLoaded", function () {
+	const deleteLinks = document.querySelectorAll(".delete-link");
+const form = document.getElementById("deleteForm");
+const input = document.getElementById("deleteId");
+
+	deleteLinks.forEach(link => {
+	link.addEventListener("click", function (event) {
+		event.preventDefault();
+
+		const id = this.getAttribute("data-id");
+		const confirmed = confirm("¿Estás seguro de que querés eliminar este cliente?");
+
+		if (confirmed) {
+			input.value = id;
+			form.submit();
+		}
+	});
+	});
+});
